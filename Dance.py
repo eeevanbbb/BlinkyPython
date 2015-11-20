2ef6e707e30623fc360107487e9ce6cf9e6a4fe0
Evan Bernstein <eeevanbbb@me.com>
2015-11-19 23:30:27 -0600

    debug

--- Dance.py
+++ Dance.py
@@ -121,6 +121,7 @@ def dartInFour(blinky):
                 for i in range(x+1,150):
                     blinky.sendPixel(0,0,0)
                 blinky.show()
+                print "Sleep for " + str(bpmTime / (dartRight - dartLeft))
                 time.sleep(bpmTime / (dartRight - dartLeft))
                 if G.keepGoing is False:
                     G.keepGoing = True
