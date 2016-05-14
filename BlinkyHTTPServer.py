9e9a839877ec92eb268dbbddac569e4068650e69
Evan Bernstein <eeevanbbb@me.com>
2016-05-13 22:13:08 -0500

    separate out dyna color

--- BlinkyHTTPServer.py
+++ BlinkyHTTPServer.py
@@ -337,7 +337,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 #See https://github.com/agusmakmun/server-jinja2/blob/master/server.py
                 env = Environment(loader=PackageLoader('app','templates'))
                 template = env.get_template('index.html')
-                s.wfile.write(template.render(commands=command_list,currentCommand=GlobalSettings.command,currentSpeed=str(GlobalSettings.speed)))
+                s.wfile.write(template.render(commands=command_list,currentCommand=GlobalSettings.command,currentSpeed=str(GlobalSettings.speed),dynamicColor=str(GlobalSettings.dynaColor)))
             else:
                 s.wfile.write("<h1>Invalid Route</h1>")
 
